18cec185befd82d7f0d221b3b6ead8aa4855f633
mirkos-vf <xxxigrik@mail.ru>
2018-07-17 16:14:02 +0300

    usart

--- usart/inc/main.h
+++ usart/inc/main.h
@@ -1,6 +1,8 @@
 #ifndef USART_MAIN_H
 #define USART_MAIN_H
 
+#define F_CPU 8000000UL
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -9,5 +11,10 @@
 #include <stdbool.h>
 
 #include "usart.h"
+
+#define CR 0x0d
+#define LF 0x0a
+
+enum state {OUTPUT = 0x00, INPUT = 0xFF};
 
 #endif //USART_MAIN_H
--- usart/src/main.c
+++ usart/src/main.c
@@ -1,24 +1,35 @@
 #include "main.h"
 
-void send_ok(void);
 
-int main(void)
-{
-    USART_Init(8); //115200
-    send_ok();
-
-    while(true)
-    {
-
-    }
-
-    return 0;
+void PORT_Init() { ;
+    DDRB = INPUT;
+    PORTB |= (1 << PORT3);
 }
 
 void send_ok(){
     USART_Transmit('O');
     USART_Transmit('k');
     USART_Transmit('!');
-    USART_Transmit(0x0d);
-    USART_Transmit(0x0a);
+    USART_Transmit(CR);
+    USART_Transmit(LF);
+}
+
+int main(void)
+{
+    USART_Init(8); //115200
+    PORT_Init();
+    send_ok();
+    sei();
+
+    while(true)
+    {
+        PORTB ^= (1 << PORT3);
+        _delay_ms(500);
+    }
+
+    return 0;
+}
+
+ISR(USART_RXC_vect) {
+
 }
